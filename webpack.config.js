245833af6cbf7561e19ba02174d9effa14892525
chenyong <chenyong@szboanda.net>
2019-04-10 15:39:31 +0800

    webpack-dev-server配置本地服务器

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,11 @@ module.exports = {
   output: {
     path: path.join( __dirname, "/dist"), //打包后的文件存放的地方
     filename: "bundle.js" //打包后输出文件的文件名
+  },
+  devServer: {
+    contentBase: "./dist", // 本地服务器所加载文件的目录
+    port: "8888",
+    inline: true, // 文件修改后实时刷新
+    historyApiFallback: true //不跳转
   }
 }
